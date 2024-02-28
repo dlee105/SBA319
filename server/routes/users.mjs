0c37ea15259978562fe839153b3285ba4ec43a11
dlee105 <dlee105.duy@gmail.com>
2024-02-28 15:05:38 -0800

    user post/patch/delete complete

--- server/routes/users.mjs
+++ server/routes/users.mjs
@@ -3,7 +3,7 @@ import db from "../databases/connection.mjs";
 import users from "../models/usersSchema.mjs";
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/", async (req, res) => {
   // GET ALL USERS
   try {
     const allUsers = await users.find();
@@ -29,19 +29,55 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", getUserId, async (req, res) => {
   // DISPLAY SELECTED USER
-  res.json({ success: true });
+  res.send(res.user);
 });
 
-router.patch("/:id", (req, res, next) => {
+router.patch("/:id", getUserId, async (req, res) => {
   // EDIT SELECTED USER
-  res.json({ success: true });
+  if (req.body.firstName != null) {
+    res.user.firstName = req.body.firstName;
+  }
+  if (req.body.lastName != null) {
+    res.user.lastName = req.body.lastName;
+  }
+  if (req.body.email != res.user.email) {
+    res.user.email = req.body.email;
+  }
+  if (req.body.username != res.user.username) {
+    res.user.username = req.body.username;
+  }
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 });
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", getUserId, async (req, res) => {
   // DELETE SELECTED USER
-  res.json({ success: true });
+  try {
+    await res.user.deleteOne();
+    res.json({ message: "user deleted" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
+
+async function getUserId(req, res, next) {
+  let user;
+  try {
+    user = await users.findById(req.params.id);
+    if (user == null) {
+      return res.status(404).json({ message: "Users does not exist" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+  res.user = user;
+  next();
+}
 
 export default router;
 
